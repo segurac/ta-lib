a20029c215041d1d70e211c9c15cc35d17d25f90
Carlos Segura <cseguramail@gmail.com>
2020-03-05 11:34:27 +0100

    minor change

--- build_wheel.sh
+++ build_wheel.sh
@@ -2,7 +2,7 @@
 
 TALIB_PATH=$(pwd)/TALIB
 
-#./tools/build_talib_from_source.bash $TALIB_PATH
+./tools/build_talib_from_source.bash $TALIB_PATH
 
 #keep only static library, this way the generated wheel package will not need any additional dependency
 rm  $TALIB_PATH/lib/*so*
